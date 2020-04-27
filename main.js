d2cda4e504d37561fb5276bcf9d38664dbc8016d
Artem Murzo <47094239+earthddx@users.noreply.github.com>
2020-04-26 21:11:49 -0400

    changed to async

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
-const API_KEY = "YOUR_API_KEY_HERE";
+const API_KEY = "YOUR_API_KEY";
 const url = "https://api.rebrandly.com/v1/links";
 
 const inputField = document.querySelector(".input");
 const shortenButton = document.querySelector(".shorten");
 const responseField = document.querySelector(".response");
 
-const shortenUrl = () => {
+const shortenUrl = async () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({ destination: urlToShorten });
 
-  fetch(url, {
-    method: "POST",
-    //access to the Rebrandly API
-    headers: {
-      "Content-type": "application/json",
-      'apikey': API_KEY
-    },
-    body: data
-  })
-    .then(
-      (res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Request failed.");
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: data,
+      //access to the Rebrandly API
+      headers: {
+        "Content-type": "application/json",
+        apikey: API_KEY,
       },
-      (networkError) => {
-        console.log(networkError.message);
-      }
-    )
-    .then((jsonResponse) => {
-      renderResponse(jsonResponse);
     });
+    if (response.ok) {
+      const jsonRes = await response.json();
+      renderResponse(jsonRes);
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
 };
 
 // Clear page and call AJAX functions
